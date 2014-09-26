5bb9a41f3ad9bfeb14f1785065f3ddc611e47d95
Mte90 <mte90smanettator@gmail.com>
2014-09-26 15:55:09 +0200

    pylint passed

--- qasana.py
+++ qasana.py
@@ -1,60 +1,59 @@
 #!/usr/bin/python3
+# pylint: disable=fixme, line-too-long
 #Initialize PyQT
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import Qt, QSettings
+from PyQt4.QtGui import QApplication, QMainWindow, QCursor, QStandardItemModel, QStandardItem, QMessageBox, QLineEdit, QInputDialog
 from asana import asana
-from subprocess import Popen
 #The fondamental for working with python
-import os,sys,signal,argparse,time
+import os, sys, signal, argparse
 from ui_main import Ui_MainWindow
-class MainWindow ( QMainWindow , Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     #Create settings for the software
-    settings = QSettings('Mte90','QAsana')
+    settings = QSettings('Mte90', 'QAsana')
     settings.setFallbacksEnabled(False)
     version = '1.0'
     appname = 'QAsana - ' + version + ' by Mte90'
     workspaces_id = {}
+    workspace_id = ''
     projects_id = {}
+    project_id = ''
     proj_tasks_id = {}
-    
-    def __init__ ( self, parent = None ):
+    asana_api = None
+
+    def __init__(self, parent=None):
         #http://pythonadventures.wordpress.com/2013/01/10/launch-just-one-instance-of-a-qt-application/
-        wid = os.popen('xdotool search --name "' + self.appname + '"').readlines()
+        wid = self.get_pid()
         if_focus = os.popen('xdotool getactivewindow').readlines()
-        #Check if multiple rows output
-        if len(wid) > 0:
-            wid = wid[0]
         #hide if the window have focues when executed again
         if if_focus[0] == wid:
             os.system('xdotool windowunmap "' + wid + '"')
             sys.exit()
-        mouse = QCursor.pos()
         #Software already opened
         if wid:
             #Show
             os.system('xdotool windowmap ' + wid)
             #get focus
             os.system('xdotool windowactivate ' + wid)
-            #move under cursor
-            os.system('xdotool getactivewindow windowmove ' + str(mouse.x() - 50) + ' ' + str(mouse.y() - 50))
+            move_under_cursor()
             sys.exit()
         else:
             #Load the ui
-            QMainWindow.__init__( self, parent )
+            QMainWindow.__init__(self, parent)
             self.ui = Ui_MainWindow()
-            self.ui.setupUi( self )
+            self.ui.setupUi(self)
             #Set the MainWindow Title
             self.setWindowTitle(self.appname)
             #Connect the function with the signal
             self.ui.pushSettings.clicked.connect(self.openKeyDialog)
             self.ui.pushRefresh.clicked.connect(self.loadAsana)
             self.ui.pushAddTask.clicked.connect(self.addTask)
-            self.ui.pushUpdate.clicked.connect(self.updateTaskList)
+            self.ui.pushUpdate.clicked.connect(self.comboProjectChanged)
             self.ui.lineTask.returnPressed.connect(self.addTask)
             self.ui.comboWorkspace.currentIndexChanged.connect(self.comboWorkspaceChanged)
             self.ui.comboProject.currentIndexChanged.connect(self.comboProjectChanged)
             #When the software are closed on console the software are closed
             signal.signal(signal.SIGINT, signal.SIG_DFL)
+            #Add hide parameter
             parser = argparse.ArgumentParser()
             parser.add_argument("--hide", help="hide the window at startup", action="store_true")
             args = parser.parse_args()
@@ -62,26 +61,22 @@ class MainWindow ( QMainWindow , Ui_MainWindow):
             QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
             self.show()
             if not args.hide:
-                #move under cursor
-                os.system('xdotool getactivewindow windowmove ' + str(mouse.x() - 50) + ' ' + str(mouse.y() - 50))
+                move_under_cursor()
             else:
-                wid = os.popen('xdotool search --name "' + self.appname + '"').readlines()
-                #Check if multiple rows output
-                if len(wid) > 0:
-                    wid = wid[0]
+                wid = self.get_pid()
                 os.popen('xdotool windowunmap "' + wid + '"')
             self.loadAsana()
-        
+
     def openKeyDialog(self):
-        key, ok = QInputDialog.getText(self, 'Authorization Key', 'Insert the key:',QLineEdit.Normal,self.settings.value('Key'))
+        key, ok = QInputDialog.getText(self, 'Authorization Key', 'Insert the key:', QLineEdit.Normal, self.settings.value('Key'))
         if not self.settings.value('Key'):
-            self.settings.setValue('Key',key)
-        
+            self.settings.setValue('Key', key)
+
     def loadAsana(self):
         if self.settings.value('Key') != -1:
             self.ui.comboWorkspace.currentIndexChanged.disconnect(self.comboWorkspaceChanged)
             QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
-            self.asana_api = asana.AsanaAPI(self.settings.value('Key'), debug=True)
+            self.asana_api = asana.AsanaAPI(self.settings.value('Key'))
             #get workspace
             workspace = self.asana_api.list_workspaces()
             self.ui.comboWorkspace.clear()
@@ -92,8 +87,8 @@ class MainWindow ( QMainWindow , Ui_MainWindow):
             self.ui.comboWorkspace.currentIndexChanged.connect(self.comboWorkspaceChanged)
             self.comboWorkspaceChanged()
         else:
-            QMessageBox.critical(self.window(), "Key not configured","Set the key for access to Asana!")
-    
+            QMessageBox.critical(self.window(), "Key not configured", "Set the key for access to Asana!")
+
     def comboWorkspaceChanged(self):
         self.ui.comboProject.currentIndexChanged.disconnect(self.comboProjectChanged)
         self.workspace_id = self.workspaces_id[self.ui.comboWorkspace.currentText()]
@@ -106,7 +101,7 @@ class MainWindow ( QMainWindow , Ui_MainWindow):
             self.ui.comboProject.addItem(i['name'])
         self.ui.comboProject.currentIndexChanged.connect(self.comboProjectChanged)
         self.comboProjectChanged()
-    
+
     def comboProjectChanged(self):
         if len(self.ui.comboProject.currentText()) > 0:
             self.project_id = self.projects_id[self.ui.comboProject.currentText()]
@@ -125,25 +120,33 @@ class MainWindow ( QMainWindow , Ui_MainWindow):
             self.ui.listTasks.setModel(qsubtasks)
             QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
             qsubtasks.itemChanged.connect(self.checkTasks)
-        
+
     def checkTasks(self, item):
         QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
         self.asana_api.rm_project_task(self.projects_id[item.text()], self.project_id)
         self.ui.listTasks.model().removeRow(item.row())
         QApplication.setOverrideCursor(QCursor(Qt.ArrowCursor))
-        
+
     def addTask(self):
         task = self.ui.lineTask.text()
         self.ui.lineTask.setText('')
         self.asana_api.create_task(task, self.workspace_id, 'me', None, False, None, None, None, [self.project_id])
-        
-    def updateTaskList(self):
-        self.comboProjectChanged()
-        
+
     #fix the include_archived not supported on get_project_tasks
-    def asana_get_project_tasks(self,project_id):
+    def asana_get_project_tasks(self, project_id):
         return self.asana_api._asana('projects/' + str(project_id) + '/tasks?completed_since=now')
-            
+
+    def get_pid(self):
+        wid = os.popen('xdotool search --name "' + self.appname + '"').readlines()
+        #Check if multiple rows output
+        if len(wid) > 0:
+            wid = wid[0]
+        return wid
+
+def move_under_cursor():
+    mouse = QCursor.pos()
+    os.system('xdotool getactivewindow windowmove ' + str(mouse.x() - 50) + ' ' + str(mouse.y() - 50))
+
 def main():
     #Start the software
     app = QApplication(sys.argv)
@@ -152,6 +155,6 @@ def main():
     ui.setupUi(MainWindow_)
     #Add the close feature at the program with the X
     sys.exit(app.exec_())
-    
+
 #Execute the software
-main() 
+main()
